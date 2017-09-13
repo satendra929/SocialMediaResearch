90762ecf5d40975ad02237364715cd14f7c0680d
satendra929 <satendra929@gmail.com>
2017-09-13 16:53:06 -0400

    Working for initial comments

--- YSMR.py
+++ YSMR.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python
 
 # Usage example:
-# python comment_threads.py --channelid='<channel_id>' --videoid='<video_id>' --text='<text>'
+# python comments.py --videoid='<video_id>' --text='<text>'
 
 import httplib2
 import os
 import sys
-
+import io
 from apiclient.discovery import build_from_document
 from apiclient.errors import HttpError
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import argparser, run_flow
 
+
+f=io.open("Comments.txt","w+",encoding='utf8')
 
 # The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
 
@@ -25,7 +27,7 @@ from oauth2client.tools import argparser, run_flow
 #   https://developers.google.com/youtube/v3/guides/authentication
 # For more information about the client_secrets.json file format, see:
 #   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
-CLIENT_SECRETS_FILE = "client_secrets.json"
+CLIENT_SECRETS_FILE = "client_secret.json"
 
 # This OAuth 2.0 access scope allows for full read/write access to the
 # authenticated user's account and requires requests to use an SSL connection.
@@ -67,12 +69,11 @@ def get_authenticated_service(args):
     return build_from_document(doc, http=credentials.authorize(httplib2.Http()))
 
 
-# Call the API's commentThreads.list method to list the existing comments.
-def get_comments(youtube, video_id, channel_id):
+# Call the API's commentThreads.list method to list the existing comment threads.
+def get_comment_threads(youtube, video_id):
   results = youtube.commentThreads().list(
     part="snippet",
     videoId=video_id,
-    channelId=channel_id,
     textFormat="plainText"
   ).execute()
 
@@ -80,82 +81,117 @@ def get_comments(youtube, video_id, channel_id):
     comment = item["snippet"]["topLevelComment"]
     author = comment["snippet"]["authorDisplayName"]
     text = comment["snippet"]["textDisplay"]
-    print "Comment by %s: %s" % (author, text)
+    f.write("Comment by %s: %s" % (author, text)+'\n')
+    #print "Comment by %s: %s" % (author, text)
 
   return results["items"]
 
 
-### Call the API's commentThreads.insert method to insert a comment.
-##def insert_comment(youtube, channel_id, video_id, text):
-##  insert_result = youtube.commentThreads().insert(
-##    part="snippet",
-##    body=dict(
-##      snippet=dict(
-##        channelId=channel_id,
-##        videoId=video_id,
-##        topLevelComment=dict(
-##          snippet=dict(
-##            textOriginal=text
-##          )
-##        )
-##      )
-##    )
-##  ).execute()
-##
-##  comment = insert_result["snippet"]["topLevelComment"]
-##  author = comment["snippet"]["authorDisplayName"]
-##  text = comment["snippet"]["textDisplay"]
-##  print "Inserted comment for %s: %s" % (author, text)
-##
+# Call the API's comments.list method to list the existing comment replies.
+def get_comments(youtube, parent_id):
+  results = youtube.comments().list(
+    part="snippet",
+    parentId=parent_id,
+    textFormat="plainText"
+  ).execute()
 
-### Call the API's commentThreads.update method to update an existing comment.
-##def update_comment(youtube, comment):
-##  comment["snippet"]["topLevelComment"]["snippet"]["textOriginal"] = 'updated'
-##  update_result = youtube.commentThreads().update(
-##    part="snippet",
-##    body=comment
-##  ).execute()
-##
-##  comment = update_result["snippet"]["topLevelComment"]
-##  author = comment["snippet"]["authorDisplayName"]
-##  text = comment["snippet"]["textDisplay"]
-##  print "Updated comment for %s: %s" % (author, text)
+  for item in results["items"]:
+    author = item["snippet"]["authorDisplayName"]
+    text = item["snippet"]["textDisplay"]
+    f.write("Comment by %s: %s" % (author, text)+'\n')
+    #print "Comment by %s: %s" % (author, text)
+
+  return results["items"]
+
+
+# Call the API's comments.insert method to reply to a comment.
+# (If the intention is to create a new to-level comment, commentThreads.insert
+# method should be used instead.)
+def insert_comment(youtube, parent_id, text):
+  insert_result = youtube.comments().insert(
+    part="snippet",
+    body=dict(
+      snippet=dict(
+        parentId=parent_id,
+        textOriginal=text
+      )
+    )
+  ).execute()
+
+  author = insert_result["snippet"]["authorDisplayName"]
+  text = insert_result["snippet"]["textDisplay"]
+  print "Replied to a comment for %s: %s" % (author, text)
+
+
+# Call the API's comments.update method to update an existing comment.
+def update_comment(youtube, comment):
+  comment["snippet"]["textOriginal"] = 'updated'
+  update_result = youtube.comments().update(
+    part="snippet",
+    body=comment
+  ).execute()
+
+  author = update_result["snippet"]["authorDisplayName"]
+  text = update_result["snippet"]["textDisplay"]
+  print "Updated comment for %s: %s" % (author, text)
+
+
+# Call the API's comments.setModerationStatus method to set moderation status of an
+# existing comment.
+def set_moderation_status(youtube, comment):
+  youtube.comments().setModerationStatus(
+    id=comment["id"],
+    moderationStatus="published"
+  ).execute()
+
+  print "%s moderated succesfully" % (comment["id"])
+
+
+# Call the API's comments.markAsSpam method to mark an existing comment as spam.
+def mark_as_spam(youtube, comment):
+  youtube.comments().markAsSpam(
+    id=comment["id"]
+  ).execute()
+
+  print "%s marked as spam succesfully" % (comment["id"])
+
+
+# Call the API's comments.delete method to delete an existing comment.
+def delete_comment(youtube, comment):
+  youtube.comments().delete(
+    id=comment["id"]
+  ).execute()
+
+  print "%s deleted succesfully" % (comment["id"])
 
 
 if __name__ == "__main__":
-  # The "channelid" option specifies the YouTube channel ID that uniquely
-  # identifies the channel for which the comment will be inserted.
-  argparser.add_argument("--channelid",
-    help="Required; ID for channel for which the comment will be inserted.")
   # The "videoid" option specifies the YouTube video ID that uniquely
   # identifies the video for which the comment will be inserted.
   argparser.add_argument("--videoid",
     help="Required; ID for video for which the comment will be inserted.")
   # The "text" option specifies the text that will be used as comment.
-  argparser.add_argument("--text", help="Required; text that will be used as comment.")
+  #argparser.add_argument("--text", help="Required; text that will be used as comment.")
   args = argparser.parse_args()
 
-  if not args.channelid:
-    exit("Please specify channelid using the --channelid= parameter.")
   if not args.videoid:
     exit("Please specify videoid using the --videoid= parameter.")
-  if not args.text:
-    exit("Please specify text using the --text= parameter.")
+##  if not args.text:
+##    exit("Please specify text using the --text= parameter.")
 
   youtube = get_authenticated_service(args)
+  # All the available methods are used in sequence just for the sake of an example.
   try:
-    # All the available methods are used in sequence just for the sake of an example.
-    # Insert channel comment by omitting videoId
-    insert_comment(youtube, args.channelid, None, args.text)
-    # Insert video comment
-    insert_comment(youtube, args.channelid, args.videoid, args.text)
-    video_comments = get_comments(youtube, args.videoid, None)
-    if video_comments:
-      update_comment(youtube, video_comments[0])
-    channel_comments = get_comments(youtube, None, args.channelid)
-    if channel_comments:
-      update_comment(youtube, channel_comments[0])
+    video_comment_threads = get_comment_threads(youtube, args.videoid)
+    parent_id = video_comment_threads[0]["id"]
+##    insert_comment(youtube, parent_id, args.text)
+    video_comments = get_comments(youtube, parent_id)
+    f.close()
+##    update_comment(youtube, video_comments[0])
+##    set_moderation_status(youtube, video_comments[0])
+##    mark_as_spam(youtube, video_comments[0])
+##    delete_comment(youtube, video_comments[0])
   except HttpError, e:
     print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
   else:
-    print "Inserted, listed and updated top-level comments."
+    print "Inserted, listed, updated, moderated, marked and deleted comments."
